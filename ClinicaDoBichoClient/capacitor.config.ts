6f9952f71588d3bd5a0a3b2ef17cbd4703e708a9
eticialima <leticialimacgi@gamil.com>
2025-08-31 18:56:58 -0300

    Cria App vazio do ionic

---
+++ ClinicaDoBichoClient/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ClinicaDoBichoClient',
+  webDir: 'www'
+};
+
+export default config;
